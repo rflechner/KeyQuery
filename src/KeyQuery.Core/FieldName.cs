59c20cc7e79fc3ccf82d4a28b1e2c06a1c2d8f3f
rflechner <rflechner@romcyber.com>
2018-07-19 08:58:47 +0200

    DataStore

--- src/KeyQuery.Core/FieldName.cs
+++ src/KeyQuery.Core/FieldName.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
@@ -76,7 +77,7 @@ namespace KeyQuery.Core
         Task<TValue> AddOrUpdate(TKey key, TValue value, Func<TKey, TValue, TValue> updateWith);
         Task<TValue> GetOrAdd(TKey key, Func<TKey, TValue> addWith);
         Task<bool> TryAdd(TKey key, TValue value);
-        Task<(bool, TValue)> TryRemove(TKey key, TValue value);
+        Task<(bool success, TValue value)> TryRemove(TKey key);
         Task<TValue> Get(TKey key);
         Task<ICollection<TKey>> AllKeys();
         Task<ICollection<TValue>> AllValues();
@@ -86,7 +87,7 @@ namespace KeyQuery.Core
     {
         private readonly ConcurrentDictionary<TKey, TValue> memory = new ConcurrentDictionary<TKey, TValue>();
 
-        public Task<TValue> AddOrUpdate(TKey key, TValue value, Func<TKey, TValue, TValue> updateWith) 
+        public Task<TValue> AddOrUpdate(TKey key, TValue value, Func<TKey, TValue, TValue> updateWith)
             => Task.FromResult(memory.AddOrUpdate(key, value, updateWith));
 
         public Task<TValue> GetOrAdd(TKey key, Func<TKey, TValue> addWith)
@@ -95,13 +96,13 @@ namespace KeyQuery.Core
         public Task<bool> TryAdd(TKey key, TValue value)
             => Task.FromResult(memory.TryAdd(key, value));
 
-        public Task<(bool, TValue)> TryRemove(TKey key, TValue value)
+        public Task<(bool success, TValue value)> TryRemove(TKey key)
         {
             var r = memory.TryRemove(key, out var v);
-            return Task.FromResult((r,v));
+            return Task.FromResult((r, v));
         }
 
-        public Task<TValue> Get(TKey key) 
+        public Task<TValue> Get(TKey key)
             => Task.FromResult(memory[key]);
 
         public Task<ICollection<TKey>> AllKeys()
@@ -115,18 +116,18 @@ namespace KeyQuery.Core
     {
         public IndexStore()
         {
-            Stores = new ConcurrentDictionary<FieldName, IAsyncKeyValueStore<FieldValue, HashSet<TId>>>();
+            Stores = new ConcurrentDictionary<FieldName, IAsyncKeyValueStore<FieldValue, ImmutableHashSet<TId>>>();
         }
 
-        public ConcurrentDictionary<FieldName, IAsyncKeyValueStore<FieldValue, HashSet<TId>>> Stores { get; }
+        public ConcurrentDictionary<FieldName, IAsyncKeyValueStore<FieldValue, ImmutableHashSet<TId>>> Stores { get; }
     }
 
     public class DataStore<TId, T>
         where T : IDto<TId>
     {
-        private IAsyncKeyValueStore<TId, T> records;
-        private IndexStore<TId> indexes;
-        private IDictionary<string, Func<T, string>> indexValueProviders;
+        private readonly IAsyncKeyValueStore<TId, T> records;
+        private readonly IndexStore<TId> indexes;
+        private readonly IDictionary<string, Func<T, string>> indexValueProviders;
 
         public DataStore(IAsyncKeyValueStore<TId, T> records, IndexStore<TId> indexes, IDictionary<string, Func<T, string>> indexValueProviders)
         {
@@ -136,8 +137,8 @@ namespace KeyQuery.Core
         }
 
         public static async Task<DataStore<TId, T>> Build(
-            Func<IAsyncKeyValueStore<TId,T>> buildPersistence,
-            Func<string, Task<IAsyncKeyValueStore<FieldValue, HashSet<TId>>>> buildFieldIndexPersistence,
+            Func<IAsyncKeyValueStore<TId, T>> buildPersistence,
+            Func<string, Task<IAsyncKeyValueStore<FieldValue, ImmutableHashSet<TId>>>> buildFieldIndexPersistence,
             params Expression<Func<T, string>>[] indexedMembers)
         {
             var indexes = new IndexStore<TId>();
@@ -155,7 +156,61 @@ namespace KeyQuery.Core
             return new DataStore<TId, T>(buildPersistence(), indexes, indexedFields.ToDictionary(kv => kv.Item1, kv => kv.Item2));
         }
 
+        public async Task<bool> Insert(T record)
+        {
+            if (!await records.TryAdd(record.Id, record))
+                return false;
 
+            foreach (var index in indexValueProviders)
+            {
+                var value = index.Value(record);
+                await Index(record.Id, index.Key, value);
+            }
+
+            return false;
+        }
+
+        public async Task Index(TId id, FieldName name, object value)
+        {
+            var index = indexes.Stores[name];
+            var fieldValue = value?.ToString();
+            var currentIds = await index.GetOrAdd(fieldValue, _ => ImmutableHashSet<TId>.Empty);
+            var ids = currentIds.Add(id);
+            await index.AddOrUpdate(fieldValue, ids, (_, set) => ids);
+        }
+
+        public async Task<ICollection<T>> SearchByIndex(FieldName name, object value)
+        {
+            var index = indexes.Stores[name];
+            var fieldValue = value?.ToString();
+            var ids = await index.GetOrAdd(fieldValue, _ => ImmutableHashSet<TId>.Empty);
+            return await Task.WhenAll(ids
+                    .Select(id => records.Get(id))
+                    .ToArray());
+        }
+
+        public async Task RemoveIndexedValue(TId id, FieldName name, object value)
+        {
+            var index = indexes.Stores[name];
+            var fieldValue = value?.ToString();
+            var currentIds = await index.GetOrAdd(fieldValue, _ => ImmutableHashSet<TId>.Empty);
+            var ids = currentIds.Remove(id);
+            await index.AddOrUpdate(fieldValue, ids, (_, set) => ids);
+        }
+
+        public async Task<bool> Remove(TId id)
+        {
+            (bool success, T record) = await records.TryRemove(id);
+            if (!success)
+                return false;
+
+            foreach (var kv in indexValueProviders)
+            {
+                var value = kv.Value(record);
+                await RemoveIndexedValue(record.Id, kv.Key, value);
+            }
+
+            return true;
+        }
     }
-
 }
