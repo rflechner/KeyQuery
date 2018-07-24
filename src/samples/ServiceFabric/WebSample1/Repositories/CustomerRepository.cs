19225b3b8f54112a0d1d87cf9403bbd9b7c9a18e
rflechner <rflechner@romcyber.com>
2018-07-24 23:16:32 +0200

    start branch

--- src/samples/ServiceFabric/WebSample1/Repositories/CustomerRepository.cs
+++ src/samples/ServiceFabric/WebSample1/Repositories/CustomerRepository.cs
@@ -3,57 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using KeyQuery.Core;
+using System.Transactions;
 using Microsoft.ServiceFabric.Data;
 using Microsoft.ServiceFabric.Data.Collections;
 
 namespace WebSample1.Repositories
 {
-  public interface ICustomerRepository
-  {
-    Task<ICollection<Customer>> GetAllCustomers(CancellationToken cancellationToken = default);
-    Task<Customer> Get(Guid id);
-    Task<bool> Insert(Customer customer);
-  }
-  
-  public class CustomerRepository : ICustomerRepository
-  {
-    private readonly IReliableStateManager stateManager;
-    private readonly DataStore<Guid, Customer> store;
-
-    public CustomerRepository(IReliableStateManager stateManager, DataStore<Guid, Customer> store)
+    public class CustomerRepository : ICustomerRepository
     {
-      this.stateManager = stateManager;
-      this.store = store;
-    }
+        private readonly IReliableStateManager stateManager;
+        private readonly DataStore<Guid, Customer> store;
 
-    public async Task<Customer> Get(Guid id)
-    {
-      return await store.FindOne(x => x.Where(c => c.Id == id));
-    }
-
-    public Task<bool> Insert(Customer customer)
-    {
-      return store.Insert(customer);
-    }
-
-    public async Task<ICollection<Customer>> GetAllCustomers(CancellationToken cancellationToken = default)
-    {
-      var customers = await stateManager.GetOrAddAsync<IReliableDictionary<Guid, Customer>>("Customer");
-
-      var results = new List<Customer>();
-
-      using (var tx = stateManager.CreateTransaction())
-      {
-        var enumerable = await customers.CreateEnumerableAsync(tx);
-        var enumerator = enumerable.GetAsyncEnumerator();
-        while (await enumerator.MoveNextAsync(cancellationToken) && !cancellationToken.IsCancellationRequested)
+        public CustomerRepository(IReliableStateManager stateManager, DataStore<Guid, Customer> store)
         {
-          results.Add(enumerator.Current.Value);
+            this.stateManager = stateManager;
+            this.store = store;
         }
-      }
 
-      return results;
+        public async Task<Customer> Get(Guid id)
+        {
+            return await store.FindOne(x => x.Where(c => c.Id == id));
+        }
+
+        public async Task<bool> Insert(Customer customer)
+        {
+            using (var tx = new TransactionScope())
+            {
+                var r = await store.Insert(customer);
+
+                tx.Complete();
+
+                return r;
+            }
+        }
+
+        public async Task<ICollection<Customer>> GetAllCustomers(CancellationToken cancellationToken = default)
+        {
+            var customers = await stateManager.GetOrAddAsync<IReliableDictionary<Guid, Customer>>("Customer");
+
+            var results = new List<Customer>();
+
+            using (var tx = stateManager.CreateTransaction())
+            {
+                var enumerable = await customers.CreateEnumerableAsync(tx);
+                var enumerator = enumerable.GetAsyncEnumerator();
+                while (await enumerator.MoveNextAsync(cancellationToken) && !cancellationToken.IsCancellationRequested)
+                {
+                    results.Add(enumerator.Current.Value);
+                }
+            }
+
+            return results;
+        }
     }
-  }
 }
---
+++ src/samples/ServiceFabric/WebSample1/Repositories/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSample1.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<ICollection<Customer>> GetAllCustomers(CancellationToken cancellationToken = default);
+        Task<Customer> Get(Guid id);
+        Task<bool> Insert(Customer customer);
+    }
+}
